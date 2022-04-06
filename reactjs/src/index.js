66d9790113394cfc0ddfb8210246d6959d1fd5da
EnriqueSource <enriqueml@blogdeenrique.com>
2022-04-06 21:58:21 +0200

    Auxiliary functions of the component close #2

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 
 const Hello = (props) => {
   console.log(props);
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  }
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }

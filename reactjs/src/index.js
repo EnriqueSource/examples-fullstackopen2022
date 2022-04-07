e13c39003ad633cb5437fc79ca12e5e75ffc2d69
EnriqueSource <enriqueml@blogdeenrique.com>
2022-04-07 22:04:46 +0200

    Page re-rendering close #6

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = ({name, age}) => {
-  console.log({name, age});
-
-  const bornYear = () => new Date().getFullYear()- age;
-
+const App = (props) => {
+  console.log(props);
+  const {counter} = props;
   return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
+} 
+
+let counter = 1;
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />,
+document.getElementById('root')
+);
 }
 
-const App = () => {
-  const name = 'Arto Hellas';
-  const age = 35;
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+refresh();
+counter += 1;
+refresh();
+counter += 1;
+refresh();

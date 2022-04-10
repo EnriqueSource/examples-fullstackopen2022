d1f99c94051b4c5721dae9161277403ef857a3d1
EnriqueSource <enriqueml@blogdeenrique.com>
2022-04-10 12:12:18 +0200

    Events handling close #9

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -2,23 +2,19 @@ import React, {useState} from 'react'; // Import useState
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  // Declare a new state variable, which we'll call "count"
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
-  // The application calls the setTimeout JSfunction and
-  // passes it two parameters: a function to increment
-  // the state of the counter and a timeout of one second.
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  // register the values of the component variables
-  // in the console. 
-  console.log('rendering...', counter)
+  const handleClick = () => {
+    console.log('clicked');
+  }
 
   return(
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>
+        plus
+      </button>
+    </div>
   )
 } 
 

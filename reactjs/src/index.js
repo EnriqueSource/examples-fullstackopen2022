ca2543497c3d1b5a92bad1ca9848fe441c9d6405
EnriqueSource <enriqueml@blogdeenrique.com>
2022-04-15 11:37:47 +0200

    Saving the left and right button click count in a single object close #17

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,19 +3,40 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
+  // Creamos una constante que es un array con la
+  // variable "clicks" y la función "setClicks". Le asignamos
+  // la función "useState()", dentro de la cual creamos
+  // el objeto que contiene el recuento de clicks de los
+  // botones "left" y Right".
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  });
 
+  // Manejador de eventos para click izquierdo
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks);
+  }
+
+  // Manejador de eventos para click derecho
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks);
+  }
+
+  // Retornamos lo que se verá en pantalla
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
-      {right}
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 } 

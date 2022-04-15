7551f24b0fdf38c73cef20fd604a69bfab643856
EnriqueSource <enriqueml@blogdeenrique.com>
2022-04-15 10:56:25 +0200

    Complex state close #16

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,36 +1,21 @@
 import React, {useState} from 'react'; // Import useState
 import ReactDOM from 'react-dom';
 
-const Display =({counter}) => <div>{counter}</div>
-
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-) 
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter -1);
-  const setToZero = () => setCounter(0);
-
-  return(
+  return (
     <div>
-      <Display counter={counter} />
-      <Button
-        handleClick={increaseByOne}
-        text='Plus'
-      />
-      <Button
-        handleClick={setToZero}
-        text='Zero'
-      />
-      <Button
-        handleClick={decreaseByOne}
-        text='Minus'
-      />
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
 } 

efc34d3b4b3b93e5e0faf5f66a4ff3c14e607d93
EnriqueSource <enriqueml@blogdeenrique.com>
2022-05-04 22:10:17 +0200

    An event handler that sets the state of the component to a given value. close #30

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom';
 const App = () => {
   const [value, setValue] = useState(10);
 
-  const hello = (who) => () => {
-      console.log('hello world', who);
+  const setToValue = (newValue) => () => {
+    setValue(newValue);
   }
 
   return (
     <div>
       {value}
-      <button onClick={hello('world')}>button</button>
-      <button onClick={hello('react')}>button</button>
-      <button onClick={hello('function')}>button</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }

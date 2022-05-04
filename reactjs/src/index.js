5d417db46f886e16652da3a74671975d60d7e6c5
EnriqueSource <enriqueml@blogdeenrique.com>
2022-05-04 21:27:13 +0200

    refactoring the definition of the event handler functions: part 3 close #29

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -4,10 +4,9 @@ import ReactDOM from 'react-dom';
 const App = () => {
   const [value, setValue] = useState(10);
 
-  const hello = (who) =>
-    () => {
+  const hello = (who) => () => {
       console.log('hello world', who);
-    }
+  }
 
   return (
     <div>

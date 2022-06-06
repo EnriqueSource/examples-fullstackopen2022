1d0773b44f744829c4eb51a6b221cd68aea93b3d
EnriqueSource <enriqueml@blogdeenrique.com>
2022-06-06 10:09:29 +0200

    App notes: filtering of important elements close #42

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -4,6 +4,7 @@ import Note from "./components/Note";
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
   // Event handler
   const addNote = (event) => {
@@ -24,9 +25,21 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
       <ul>
         {notes.map((note) => (
           <Note key={note.id} note={note} />
